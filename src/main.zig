dda711c137060b8428904d39f559f8cfdab2d291
Rekai Musuka <rekai@musuka.dev>
2023-04-11 02:01:32 -0500

    chore: update main
    this is basicallly just test code I can attatch to gdb

--- src/main.zig
+++ src/main.zig
@@ -5,6 +5,22 @@ pub fn main() !void {
     var trie = try HashArrayMappedTrie.init(std.heap.page_allocator);
     defer trie.deinit();
 
-    try trie.insert("hello", {});
-    try trie.insert("helloworld", {});
+    try trie.insert("and", {});
+    try trie.insert("class", {});
+    try trie.insert("else", {});
+    try trie.insert("false", {});
+    try trie.insert("for", {});
+    try trie.insert("fun", {});
+    try trie.insert("if", {});
+    try trie.insert("nil", {});
+    try trie.insert("or", {});
+    try trie.insert("print", {});
+    try trie.insert("return", {});
+    try trie.insert("super", {});
+    try trie.insert("this", {});
+    try trie.insert("true", {});
+    try trie.insert("var", {});
+    try trie.insert("while", {});
+
+    trie.walk();
 }
